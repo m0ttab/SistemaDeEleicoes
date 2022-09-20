e4596582a12fc9f599a14bdba2841d7844d02027
Brandon <104904323+Brxndin@users.noreply.github.com>
2022-09-20 15:51:24 -0300

    Add files via upload

--- views/votacao/create.blade.php
+++ views/votacao/create.blade.php
@@ -7,50 +7,31 @@
 @if($permissao)
 
 <form method="POST" action="/eleicoes/public/votacao/store" id="form">
+  
   <input type="hidden" name="_token" value="{{csrf_token()}}">
   <div class="form-group">
     <input class="form-control" type="text" name="titulo" placeholder="Informe seu titulo de eleitor">
   </div>
   <div class="form-group">
-    <input class="form-control" type="number" name="candidato" placeholder="Informe o número do seu candidato">
+    <input class="form-control" type="number" name="presidente" placeholder="Informe o número do seu candidato a presidente">
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="number" name="governador" placeholder="Informe o número do seu candidato a governador">
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="number" name="senador" placeholder="Informe o número do seu candidato a senador">
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="number" name="deputado_estadual" placeholder="Informe o número do seu candidato a deputado estadual">
+  </div>
+  <div class="form-group">
+    <input class="form-control" type="number" name="deputado_federal" placeholder="Informe o número do seu candidato a deputado federal">
   </div>
   <div class="form-group">
     <button class="btn btn-warning" type="submit">Enviar</button>
     <button class="btn btn-danger" type='reset'>Cancelar</button>
   </div>
 </form>
-</div>
-<script>
-    
-    // document.getElementById('form').onsubmit = (e) => {
-      
-    //     e.preventDefault();
-    //     var form_data = new FormData(document.getElementById('form'));
-        
-    //     fetch('/votacao/store', {
-          
-    //       method: 'POST',
-    //       body: form_data
-          
-    //     }).then((req) => {
-          
-    //       if(req.status == 200){
-
-    //         req.json().then((res) => {
-
-    //           alert(res.mensagem);
-    //           document.getElementById('form').reset();
-
-    //         });
-
-    //       }else{
-    //           alert('Erro no cadastro!');
-    //       }
-          
-    //     });
-    // }
-
-</script>
 
 @else
 
